80ae8819f1766540d3a132af6e65cfc576688a30
nliza <nurhalizah117429@gmail.com>
2023-10-08 00:16:56 +0700

    first commit

---
+++ javascript.js
@@ -0,0 +1,93 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// if
+let suhu = 35;
+if (suhu < 37) {
+    console.log("Anda sehat");
+}
+
+// if else 
+let wifi = "lemot";
+if (wifi === "lancar") {
+    console.log ("saya mau nonton spy x family");
+} else {
+    console.log ("saya mau ganti provider wifi saja");
+}
+
+// nested if 
+let mata = "ngantuk";
+let tugas = "aman";
+if (mata === "ngantuk" && tugas === "aman") {
+    console.log("mau tidur dengan nyenyak");
+} else if (mata === "ngantuk" && tugas === "numpuk") {
+    console.log("harus begadang");
+} else if (mata === "segar" && tugas === "aman") {
+    console.log("lanjut scroll Twitter");
+} else if (mata === "segar" && tugas === "numpuk") {
+    console.log("mengerjakan tugas dengan semangat");
+} else {
+    console.log("kondisi tidak terdefinisi");
+}
+
+// switch case
+rl.question('Luas bangun datar apa yang ingin anda ketahui? (persegi/segitia/persegi panjang/lingkaran): ', (pilihan) => {
+  let rumus;
+  switch (pilihan) {
+    case "persegi":
+      rumus = "sisi x sisi";
+      break;
+    case "segitia":
+      rumus = "(alas x tinggi)/2";
+      break;
+    case "persegi panjang":
+      rumus = "panjang x lebar";
+      break;
+    case "lingkaran":
+      rumus = "π*r^2";
+      break;
+    default:
+      rumus = "tidak ada";
+  }
+  console.log(`Rumus dari bangun datar ${pilihan} adalah ${rumus}`);
+  rl.close();
+});
+
+// for statement
+let total = 0;
+for (let i = 760; i <= 777; i++) {
+  total += i;
+}
+console.log("Total: " + total);
+
+//while
+let i = 17;
+while (i <= 25) {
+  if (i % 3 === 0) {
+    console.log(i);
+  }
+  i++;
+}
+
+// do while
+let angka = 61;
+do {
+  console.log(angka);
+  angka += 4;
+} while (angka <= 72);
+
+//function
+function hitungJumlahHuruf(kata) {
+  let jumlahHuruf = kata.length;
+  return jumlahHuruf;
+}
+rl.question('Masukkan sebuah kata : ', (kata) => {
+  let jumlahHuruf = hitungJumlahHuruf(kata);
+
+  console.log("Jumlah huruf dalam kata tersebut adalah : " + jumlahHuruf);
+
+  rl.close();
+});
